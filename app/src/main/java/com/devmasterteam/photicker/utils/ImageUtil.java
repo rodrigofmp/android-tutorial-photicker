db31485a6ee6ad344c62dff4e12bee43d7298d63
Rodrigo Petters <rodrigo.petters@inovan.net>
2018-04-28 19:20:42 -0300

    Criando as image view dos sticker dinamicamente

---
+++ app/src/main/java/com/devmasterteam/photicker/utils/ImageUtil.java
@@ -0,0 +1,103 @@
+package com.devmasterteam.photicker.utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.devmasterteam.photicker.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageUtil {
+    public static List<Integer> getImagesList() {
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.st_facial_0);
+        images.add(R.drawable.st_facial_1);
+        images.add(R.drawable.st_facial_3);
+        images.add(R.drawable.st_facial_4);
+        images.add(R.drawable.st_facial_5);
+        images.add(R.drawable.st_facial_6);
+        images.add(R.drawable.st_facial_7);
+        images.add(R.drawable.st_facial_8);
+        images.add(R.drawable.st_facial_10);
+        images.add(R.drawable.st_facial_11);
+        images.add(R.drawable.st_facial_12);
+        images.add(R.drawable.st_facial_13);
+        images.add(R.drawable.st_facial_14);
+        images.add(R.drawable.st_comida_1);
+        images.add(R.drawable.st_comida_2);
+        images.add(R.drawable.st_comida_3);
+        images.add(R.drawable.st_comida_5);
+        images.add(R.drawable.st_comida_6);
+        images.add(R.drawable.st_coracao_1);
+        images.add(R.drawable.st_coracao_2);
+        images.add(R.drawable.st_coracao_3);
+        images.add(R.drawable.st_coracao_4);
+        images.add(R.drawable.st_coracao_5);
+        images.add(R.drawable.st_coracao_6);
+        images.add(R.drawable.st_drink_1);
+        images.add(R.drawable.st_drink_2);
+        images.add(R.drawable.st_drink_3);
+        images.add(R.drawable.st_drink_4);
+        images.add(R.drawable.st_drink_5);
+        images.add(R.drawable.st_drink_6);
+        images.add(R.drawable.st_drink_7);
+        images.add(R.drawable.st_drink_8);
+        images.add(R.drawable.st_drink_9);
+        images.add(R.drawable.st_drink_10);
+        images.add(R.drawable.st_tatto_1);
+        images.add(R.drawable.st_tatto_2);
+        images.add(R.drawable.st_tatto_3);
+        images.add(R.drawable.st_tatto_4);
+        images.add(R.drawable.st_tatto_5);
+        images.add(R.drawable.st_tatto_6);
+        images.add(R.drawable.st_sticker_2);
+        images.add(R.drawable.st_sticker_5);
+        images.add(R.drawable.st_sticker_6);
+        images.add(R.drawable.st_sticker_7);
+        images.add(R.drawable.st_sticker_8);
+        images.add(R.drawable.st_sticker_9);
+        images.add(R.drawable.st_sticker_11);
+        return images;
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+}
--- app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -2,8 +2,13 @@ package com.devmasterteam.photicker.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.devmasterteam.photicker.R;
+import com.devmasterteam.photicker.utils.ImageUtil;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
+        List<Integer> mListImages = ImageUtil.getImagesList();
+
+        final LinearLayout content = (LinearLayout) this.findViewById(R.id.linear_horizontal_scroll_content);
+
+        for (Integer imageId: mListImages) {
+            ImageView image = new ImageView(this);
+            image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
+            image.setPadding(20,10,20,10);
+            content.addView(image);
+        }
     }
 }
