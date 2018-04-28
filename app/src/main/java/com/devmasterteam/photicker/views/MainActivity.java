56f77423999f7e4511c6ba6894ac7afcf85aa531
Rodrigo Petters <rodrigo.petters@inovan.net>
2018-04-28 19:33:34 -0300

    Criando as image view dos sticker dinamicamente

--- app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -1,9 +1,13 @@
 package com.devmasterteam.photicker.views;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.devmasterteam.photicker.R;
 import com.devmasterteam.photicker.utils.ImageUtil;
@@ -23,13 +27,40 @@ public class MainActivity extends AppCompatActivity {
 
         List<Integer> mListImages = ImageUtil.getImagesList();
 
+
+        final RelativeLayout relativeLayout = (RelativeLayout) this.findViewById(R.id.relative_photo_content_draw);
         final LinearLayout content = (LinearLayout) this.findViewById(R.id.linear_horizontal_scroll_content);
 
         for (Integer imageId: mListImages) {
             ImageView image = new ImageView(this);
             image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
             image.setPadding(20,10,20,10);
+
+            BitmapFactory.Options dimensions = new BitmapFactory.Options();
+            dimensions.inJustDecodeBounds = true; // Não usa alocação de memória para essa imagem
+            BitmapFactory.decodeResource(getResources(), imageId, dimensions);
+
+            final int width = dimensions.outWidth;
+            final int height = dimensions.outHeight;
+
+            image.setOnClickListener(onClickImageOption(relativeLayout, imageId, width, height));
+
             content.addView(image);
         }
+    }
+
+    private View.OnClickListener onClickImageOption(final RelativeLayout relativeLayout, final Integer imageId, int width, int height) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setBackgroundResource(imageId);
+                relativeLayout.addView(imageView);
+
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) imageView.getLayoutParams();
+                layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            }
+        };
     }
 }
